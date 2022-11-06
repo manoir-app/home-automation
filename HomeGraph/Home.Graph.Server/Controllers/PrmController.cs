7fdb07d81b7edfffab237218ee8931e27cfc157c
Mégane WAROQUIER <megane.waroquier@live.com>
2022-11-06 09:14:31 +0100

    Création du controlleur (vide) pour PRM

---
+++ HomeGraph/Home.Graph.Server/Controllers/PrmController.cs
@@ -0,0 +1,43 @@
+﻿using Home.Common.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MQTTnet.Extensions;
+using System.Collections.Generic;
+
+namespace Home.Graph.Server.Controllers
+{
+    [Route("api/prm")]
+    [ApiController]
+    public class PrmController : ControllerBase
+    {
+
+        public class UserPrmDataWithDetails : UserPrmData
+        {
+
+        }
+
+        [Route("search"), HttpGet]
+        public List<UserPrmDataWithDetails> GetList(string nameSearch = null, string tagSearch = null)
+        {
+            return new List<UserPrmDataWithDetails>();
+        }
+
+        [Route("users/{id}"), HttpGet]
+        public UserPrmDataWithDetails Get(string id)
+        {
+            return null;
+        }
+
+        [Route("users"), HttpPost]
+        public UserPrmDataWithDetails UpsertUserData([FromBody] UserPrmDataWithDetails account)
+        {
+            return null;
+        }
+
+        [Route("users/{id}"), HttpDelete]
+        public bool Delete(string id, bool deleteUser = false)
+        {
+            return false;
+        }
+    }
+}
