7c9f263d37b5ad465d8b1cf07af71d9598e0598e
Michael CARBENAY <michael@carbenay.info>
2023-05-09 21:35:50 +0200

    Création d'un projet de tests unitaires

---
+++ Agents/TestsForAgents/Common/AgentHelperTests.cs
@@ -0,0 +1,46 @@
+﻿using Home.Common;
+using Home.Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsForAgents.Common
+{
+    [TestClass]
+    public class AgentHelperTests
+    {
+        [TestInitialize]
+        public static void TestInitialize(TestContext testContext)
+        {
+            MainApiAgentWebClient.ClearTestHandlers();
+        }
+
+
+        [TestMethod]
+        public void RedirectionDesAppelsApi()
+        {
+            MainApiAgentWebClient.RegisterTestHandler("v1.0/users/main", new Func<List<User>>( () =>
+            {
+                return new List<User>();
+            }));
+            var usrs = AgentHelper.GetMainUsers("tests");
+            Assert.IsTrue(usrs.Count == 0);
+
+            MainApiAgentWebClient.RegisterTestHandler("v1.0/users/main", new Func<List<User>>(() =>
+            {
+                return new List<User>(new User[]
+                {
+                    new User { Id = "test", Name = "TestNom", FirstName="TestPrenom"},
+                    new User { Id = "test2", Name = "TestNom2", FirstName="TestPrenom2"},
+                });
+            }));
+            
+            usrs = AgentHelper.GetMainUsers("tests");
+            Assert.IsTrue(usrs.Count == 2);
+            Assert.AreEqual<string>("test", usrs[0].Id);
+            Assert.AreEqual<string>("test2", usrs[1].Id);
+        }
+    }
+}
--- Common/Home.Graph.Common/MainApiAgentWebClient.cs
+++ Common/Home.Graph.Common/MainApiAgentWebClient.cs
@@ -2,12 +2,29 @@
 using Newtonsoft.Json;
 using SharpCompress.Common.Rar;
 using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Reflection;
 
 namespace Home.Common
 {
     public class MainApiAgentWebClient : WebClient
     {
+        internal static void ClearTestHandlers()
+        {
+            _rewrite.Clear();
+        }
+
+        static Dictionary<string, Delegate> _rewrite = new Dictionary<string, Delegate>();
+        internal static void RegisterTestHandler<T>(string url, Func<T> function)
+        {
+            _rewrite["GET_" + url] = function;
+        }
+        internal static void RegisterTestHandler<T, R>(string url, string verb, Func<R, T> function)
+        {
+            _rewrite[verb.ToUpperInvariant() + "_" + url] = function;
+        }
+
         public MainApiAgentWebClient(string agentName)
         {
             var srv = LocalDebugHelper.GetLocalGraphHost();
@@ -22,12 +39,26 @@ namespace Home.Common
 
         public T DownloadData<T>(string url)
         {
+            if(_rewrite.ContainsKey("GET_" + url))
+            {
+                Delegate dlg = _rewrite["GET_" + url];
+                object ret = dlg.DynamicInvoke();
+                return (T) ret;
+            }
+
             var s = this.DownloadString(url);
             return JsonConvert.DeserializeObject<T>(s);
         }
 
         public T UploadData<T, R>(string url, string method, R data)
         {
+            if (_rewrite.ContainsKey(method.ToUpper() + "_" + url))
+            {
+                Delegate dlg = _rewrite[method.ToUpper() + "_" + url];
+                object ret = dlg.DynamicInvoke(data);
+                return (T)ret;
+            }
+
             var s = this.UploadString(url, method, JsonConvert.SerializeObject(data));
             return JsonConvert.DeserializeObject<T>(s);
         }
