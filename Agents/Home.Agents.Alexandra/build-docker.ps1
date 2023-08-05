11294d311fd0886d114b613c21b4c3168299d64c
Michael CARBENAY <michael@carbenay.info>
2023-08-05 11:11:45 +0200

    On retire l'intégration alexa qui n'était pas commencée :)

--- Agents/Home.Agents.Alexandra/build-docker.ps1
+++
@@ -1,9 +0,0 @@
-﻿docker build . -f ..\..\..\Dockerfile.Release -t 192.168.2.184:5000/alexandra:latest
-
-docker tag 192.168.2.184:5000/alexandra:latest 192.168.2.100:5000/alexandra:latest
-
-docker push 192.168.2.184:5000/alexandra:latest
-docker push 192.168.2.100:5000/alexandra:latest
-
-docker image rm 192.168.2.184:5000/alexandra:latest
-docker image rm 192.168.2.100:5000/alexandra:latest
