b20dea94bfa5a61061e6d43f560b91f7f09c5807
Michael CARBENAY <michael@altazion.com>
2025-01-09 08:30:18 +0100

    Création de l'auto-client trilium :)

---
+++ Common/Home.Graph.Common/Integrations/Knowledge/TrilumClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home.Graph.Common.Integrations.Knowledge
+{
+    public partial class TriliumNotesClient
+    {
+
+    }
+}
